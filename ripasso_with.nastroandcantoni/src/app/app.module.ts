39fc65ee42893105353bb74dcb2342a92b6b0b2a
Samuel28102005 <labarba.samuel@iisgalvanimi.edu.it>
2023-05-29 20:37:02 +0000

    punto 3

--- ripasso_with.nastroandcantoni/src/app/app.module.ts
+++ ripasso_with.nastroandcantoni/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
@@ -14,7 +14,8 @@ import { ItemComponent } from './item/item.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ripasso_with.nastroandcantoni/src/app/search/search.component.ts
+++ ripasso_with.nastroandcantoni/src/app/search/search.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { Root } from '../search.models';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent {
+birra!: string
+obsbirra !: Observable <Root>
+ris !: Root
+
+
+
+constructor(public http:HttpClient){
+
+}
+
+
+
+cerca(query:HTMLInputElement){
+  this.birra = query.value
+  this.obsbirra=this.http.get<Root>(`https://api.punkapi.com/v2/beers?beer_name=${query}`)
+  this.obsbirra.subscribe((data:Root)=>{this.ris=data})
+}
+
 
 }
